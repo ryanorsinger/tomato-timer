ec010b01a35f2b1bbed297d5d8882cb2d3f5794a
Paul Dowman <paul@pauldowman.com>
2015-10-20 15:33:26 -0400

    Remove insecure and add policy
    Teaching notes:
    
    Add {{#if currentUser}} around form and delete button
    > meteor remove insecure
    Try to remove
    Add userId to pomodoro
    Only find pomodoros for current user
    Add allow rules
    NOTE: we're still publishing all Pomodoros

--- collections/pomodoro.js
+++ collections/pomodoro.js
@@ -1,1 +1,11 @@
 Pomodoros = new Mongo.Collection("Pomodoros", {});
+
+var ownsDocument = function(userId, doc) {
+  return doc && doc.userId === userId;
+};
+
+Pomodoros.allow({
+  insert: ownsDocument,
+  update: ownsDocument,
+  remove: ownsDocument,
+});
