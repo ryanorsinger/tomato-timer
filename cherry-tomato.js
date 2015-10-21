f6054137e9b98f5823375da0de89dc808ba8386b
ryanorsinger <ryan.orsinger@gmail.com>
2015-10-21 10:54:23 -0700

    Add create, read, and delete timers

--- cherry-tomato.js
+++ cherry-tomato.js
@@ -1,18 +1,39 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+// Collection declaration. Notice that this is global.
+// Creates mongo collection on server
+// Creates a cache-like "minimongo" in memory on the client
+Poms = new Mongo.Collection('pomodoros', {});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+
+  Template.pomodorosList.helpers({
+    allPomodoros: function () {
+      // find().fetch() gets back the collection of objects
+      // return Poms.find().fetch();
+      return Poms.find({}, {sort: {startDate: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.pomodorosList.events({
+    'click input[value="Delete"]' : function (e) {
+      Poms.remove(this._id);
     }
+  });
+
+  // Event handler listening for #new-pomodoro form is submitted
+  Template.pomodoroInput.events({
+    'submit #new-pomodoro' : function(e) {
+      // Prevent the default form submit behavior
+      e.preventDefault();
+
+      var pomodoro = {
+        startDate: new Date(),
+        goal: e.target.goal.value
+      };
+
+      Poms.insert(pomodoro);
+
+      e.target.goal.value = '';
+    },
   });
 }
 
---
+++ notes.md
@@ -0,0 +1,2 @@
+
+Poms.find().fetch()
