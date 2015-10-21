ae3c7f9ea0e0a2bf2cdcd220521844c961a854a9
ryanorsinger <ryan.orsinger@gmail.com>
2015-10-21 11:51:27 -0700

    Always remove the insecure package

--- server/server.js
+++ server/server.js
@@ -2,4 +2,13 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  // Poms.allow({
+  //   insert: function (userId, pomodoro) {
+  //       return pomodoro.owner === userId;
+  //   },
+  //   remove: function (userId, pomodoro) {
+  //       return pomodoro.owner === userId;
+  //   }
+  // });
 }
