caa171f0724fa8123aec4e6f101e67a0985f72bc
ryanorsinger <ryan.orsinger@gmail.com>
2015-10-21 12:04:47 -0700

    Remove superfluous isclient checking from clientjs

--- client/main.js
+++ client/main.js
@@ -1,46 +1,44 @@
-if (Meteor.isClient) {
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
 
-  Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_EMAIL'
-  });
+Template.pomodorosList.helpers({
+  allPomodoros: function () {
+    // find().fetch() gets back the collection of objects
+    // return Poms.find().fetch();
+    // Poms.find({}, {sort: {startDate: -1}});
 
-  Template.pomodorosList.helpers({
-    allPomodoros: function () {
-      // find().fetch() gets back the collection of objects
-      // return Poms.find().fetch();
-      // Poms.find({}, {sort: {startDate: -1}});
+    let userId = Meteor.userId();
 
-      let userId = Meteor.userId();
-
-      if(userId !== null) {
-        return Poms.find({ owner: userId }, {sort: {startDate: -1}});
-      }
+    if(userId !== null) {
+      return Poms.find({ owner: userId }, {sort: {startDate: -1}});
     }
-  });
+  }
+});
 
-  Template.pomodorosList.events({
-    'click input[value="Delete"]' : function (e) {
-      Poms.remove(this._id);
-    }
-  });
+Template.pomodorosList.events({
+  'click input[value="Delete"]' : function (e) {
+    Poms.remove(this._id);
+  }
+});
 
-  // Event handler listening for #new-pomodoro form is submitted
-  Template.pomodoroInput.events({
-    'submit #new-pomodoro' : function(e) {
-      // Prevent the default form submit behavior
-      e.preventDefault();
+// Event handler listening for #new-pomodoro form is submitted
+Template.pomodoroInput.events({
+  'submit #new-pomodoro' : function(e) {
+    // Prevent the default form submit behavior
+    e.preventDefault();
 
-      var pomodoro = {
-        startDate: new Date(),
-        goal: e.target.goal.value,
-        owner: Meteor.userId(),
-      };
+    var pomodoro = {
+      startDate: new Date(),
+      goal: e.target.goal.value,
+      owner: Meteor.userId(),
+    };
 
-      // Insert the new object into the Poms collection.
-      Poms.insert(pomodoro);
+    // Insert the new object into the Poms collection.
+    Poms.insert(pomodoro);
 
-      // Reset the input field
-      e.target.goal.value = '';
-    },
-  });
-}
+    // Reset the input field
+    e.target.goal.value = '';
+  },
+});
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,8 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-
-  // Poms.allow({
-  //   insert: function (userId, pomodoro) {
-  //       return pomodoro.owner === userId;
-  //   },
-  //   remove: function (userId, pomodoro) {
-  //       return pomodoro.owner === userId;
-  //   }
-  // });
-}
+Poms.allow({
+  insert: function (userId, pomodoro) {
+      return pomodoro.owner === userId;
+  },
+  remove: function (userId, pomodoro) {
+      return pomodoro.owner === userId;
+  }
+});
